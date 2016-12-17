96a1a3c730b886a6b09594d36ccbfa3c86085249
Dorus Leliveld <dorusleliveld@gmail.com>
2016-12-17 17:53:38 -0600

    Fixed list decoding and encoding The list instances used previously were incorrect as the dont account for padding and do not have the correct format (prepend the size of the list). Added a test case (with a multiple packages bytestring arbitrary instance) to check for this in the future.

--- Graphics/Sudbury/WirePackages.hs
+++ Graphics/Sudbury/WirePackages.hs
@@ -11,10 +11,15 @@ module Graphics.Sudbury.WirePackages where
 
 import Data.Word
 import Data.Store
+import Data.Foldable
 import Data.Store.Core
+import Data.Store.Internal
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Internal as B
 import qualified Data.ByteString.Builder as BB
+import Control.Monad
+import Foreign.Ptr (minusPtr, plusPtr)
+
 
 
 data WirePackage = WirePackage
@@ -24,9 +29,8 @@ data WirePackage = WirePackage
   , wirePackagePayload :: B.ByteString
   } deriving (Eq, Show)
 
--- Storable that writes in the Wayland binary format.
--- Allows us to use the optimized machinery for Data.Store
--- to gain faster performing decoding/encoding.
+-- | Storable that writes in the Wayland binary format.
+-- Allows us to use the optimized machinery from Data.Store
 instance Store WirePackage where
   size = VarSize $ fromIntegral . wirePackageSize
   poke p = do
@@ -43,14 +47,55 @@ instance Store WirePackage where
     plraw <- peekToPlainForeignPtr "Data.ByteString.ByteString" payloadSize
     let pl = B.PS plraw 0 payloadSize
     return $ WirePackage 
-      { wirePackageSender=sen
-      , wirePackageSize=sz 
-      , wirePackageOpcode=op 
-      , wirePackagePayload=pl
+      { wirePackageSender  = sen
+      , wirePackageSize    = sz
+      , wirePackageOpcode  = op
+      , wirePackagePayload = pl
       }
   {-# INLINE size #-}
   {-# INLINE peek #-}
   {-# INLINE poke #-}
+
+-- | Newtype wrapper around a list of WirePackages is 
+-- necessary to write the correct amount of pad bytes
+-- and to account for non-prepending of list length
+newtype WirePackageStream = WirePackageStream 
+  { unWirePackageStream :: [WirePackage]
+  } deriving (Eq, Show)
+
+-- | Storable instance for a number of Wayland wirepackages
+-- that accounts for padding and deserializing.
+instance Store WirePackageStream where
+  size = VarSize $ foldl' sumAlignedSize 0 . unWirePackageStream
+    where
+      VarSize f = size :: Size WirePackage
+      sumAlignedSize :: Int -> WirePackage -> Int
+      sumAlignedSize acc x = let pkgSize = f x in pkgSize + acc + (pkgSize `mod` 4)
+
+  poke (WirePackageStream xs) = traverse_ pokeAlign xs
+    where
+      {-# INLINE pokeAlign #-}
+      pokeAlign wp = let align = fromIntegral $ wirePackageSize wp `mod` 4
+                         emptyByte = 0 :: Word8 -- Maybe skipPoke would be better
+                      in poke wp >> replicateM align (poke emptyByte)
+
+  peek = WirePackageStream <$> go
+    where  
+      remaining :: Peek Int
+      remaining = Peek $ \ps ptr -> return (ptr, peekStateEndPtr ps `minusPtr` ptr)
+      {-# INLINE go #-}
+      go :: Peek [WirePackage]
+      go = 
+        do bytesLeft <- remaining
+           if bytesLeft < 8 then Peek $ \_ ptr -> return (ptr, [])
+                            else do wp <- peek :: Peek WirePackage
+                                    let align = fromIntegral $ wirePackageSize wp `mod` 4
+                                    skip align
+                                    (wp :) <$> go
+  {-# INLINE size #-}
+  {-# INLINE peek #-}
+  {-# INLINE poke #-}
+
 
 wirePack :: WirePackage -> BB.Builder
 wirePack = BB.byteString . encode
--- tests/Wire.hs
+++ tests/Wire.hs
@@ -4,16 +4,30 @@ import Test.Framework
 import Test.Framework.Providers.QuickCheck2 (testProperty)
 import Test.QuickCheck
 import qualified Data.Store as S
+import qualified Data.ByteString as B
 
 import Graphics.Sudbury.WirePackages
 
-import Arbitrary ()
+import Arbitrary
 
 tests :: Test
 tests = testGroup "Wire protocol transformations"
   [ testProperty "wire packages identity" prop_package_id
+  , testProperty "wire stream encoding identity" prop_package_stream_id
+  , testProperty "wire stream decoding identity" prop_package_binary_id
   ]
 
 -- | Test if packing and then parsing a 'WirePackage' yields the original package.
 prop_package_id :: WirePackage -> Property
 prop_package_id = (===) <*> (S.decodeEx . S.encode)
+
+prop_package_stream_id :: WirePackageStream -> Property
+prop_package_stream_id = (===) <*> (WirePackageStream . S.decodeEx . S.encode . unWirePackageStream)
+
+prop_package_binary_id :: WirePackageBinary -> Property
+prop_package_binary_id = (===) <*> (WirePackageBinary . S.encode . decode' . unWpBin)
+  where
+    decode' :: B.ByteString -> WirePackageStream
+    decode' = S.decodeEx
+
+
